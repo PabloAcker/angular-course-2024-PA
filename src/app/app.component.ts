7f51785e9f8ddc4a946965539e082f73c6eed24e
PabloAcker <favw14@gmail.com>
2024-09-29 20:28:31 -0400

    [29-09-2024].insertNg-content&ngIfOldAndNewSyntaxByAcker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { UserCardComponent } from './user-card/user-card.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { HistoryCalculatorComponent } from './history-calculator/history-calculator.component';
+import { CommonModule } from '@angular/common';
 
 interface IPerson {
   name:string
@@ -13,7 +14,7 @@ interface IPerson {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserCardComponent, CalculatorComponent, HistoryCalculatorComponent],
+  imports: [RouterOutlet, UserCardComponent, CalculatorComponent, HistoryCalculatorComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
