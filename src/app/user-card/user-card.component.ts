ff19b31a0a2e60e34766692526b8e0944fd62f41
PabloAcker <favw14@gmail.com>
2024-10-01 20:34:46 -0400

    [01-10-2024].insertDoCheck&AfterContentInit

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, DoCheck, AfterContentInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: "app-user-card",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
 })
-export class UserCardComponent implements OnInit, OnDestroy, OnChanges{
+export class UserCardComponent implements OnInit, OnDestroy, OnChanges, DoCheck, AfterContentInit{
   @Input() name:string = ''
   @Input() email:string = ''
 
@@ -33,6 +34,13 @@ export class UserCardComponent implements OnInit, OnDestroy, OnChanges{
     console.log('CHANGES:', changes)
     this.password = changes['name'].currentValue + ' ' + changes['email'].currentValue + ' PASSWORD'
   }
+
+  ngDoCheck(): void {
+    console.log('DO CHECK user card')  
+   }
+   ngAfterContentInit(): void {
+       console.log('NG AFTER CONTENT INIT')
+   }
 
   public onSendData(){
     this.sendData.emit('Hi from child component')
