e3e569db75b70e5f7dad729bb7ba0aca20ba7e79
PabloAcker <favw14@gmail.com>
2024-10-14 21:08:03 -0400

    [14-10-2024] Angular material by Acker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ import { AppColorsDirective } from './app-colors.directive';
 import { CreateHtmlDirective } from './create-html.directive';
 import { PurePipe } from './pure.pipe';
 import { ImpurePipe } from './impure.pipe';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 interface IPersonn {
   name:string;
@@ -31,7 +33,9 @@ interface IPersonn {
     AppColorsDirective,
     CreateHtmlDirective,
     PurePipe,
-    ImpurePipe],
+    ImpurePipe,
+    MatCardModule,
+    MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
