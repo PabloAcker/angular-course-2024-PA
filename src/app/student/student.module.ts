f2e0a23acddc0945aa4887e969057981989ccbbd
PabloAcker <favw14@gmail.com>
2024-10-17 23:20:22 -0400

    [17-10-2024] navigation from html by Acker

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { StudentRoutingModule } from './student-routing.module';
 import { ScoreComponent } from './score/score.component';
+import { ClassmateComponent } from './classmate/classmate.component';
+import { AverageComponent } from './average/average.component';
 
 
 @NgModule({
-  declarations: [ScoreComponent],
+  declarations: [ScoreComponent, ClassmateComponent, AverageComponent],
   imports: [
     CommonModule,
     StudentRoutingModule
